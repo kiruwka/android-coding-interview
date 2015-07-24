4ce642c47b4fb0c137599e5ca552ebec84ea83f7
Kirill Moklyuk <kirill@kapaza.be>
2015-07-24 19:34:26 +0200

    little java doc update

--- app/src/main/java/android/coding/interview/makeitawesome/fragment/WelcomeScreenFragment.java
+++ app/src/main/java/android/coding/interview/makeitawesome/fragment/WelcomeScreenFragment.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * Default initial fragment
+ * Created by km on 7/24/15.
  */
 public class WelcomeScreenFragment extends Fragment {
 
